bb5c9ba4f6a46397df567b08694cf37c56600524
Sangchan Kim <sck970112@gmail.com>
2016-07-06 22:47:21 -0400

    Autofill default user/pass and make basic data getter

--- app/src/main/java/com/example/jxw679/mogul/activities/MainActivity.java
+++ app/src/main/java/com/example/jxw679/mogul/activities/MainActivity.java
@@ -16,7 +16,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import com.example.jxw679.mogul.model.User;
 import com.example.jxw679.mogul.R;
 
 
@@ -29,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText etPassword;
     private ImageButton btSignIn;
     private ImageButton btSignUp;
+
+
+    private DatabaseReference mDatabase;
+
     private static final String TAG = "LOGIN";
 
     @Override
@@ -57,15 +69,37 @@ public class MainActivity extends AppCompatActivity {
                                     Toast.makeText(MainActivity.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
                                 } else {
-                                    // Intent intent = new Intent(null, ChildView.class);
-                                    //MainActivity.this.startActivity(intent);
-                                }
 
+
+                                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                    if (user != null) {
+                                        String uid = user.getUid();
+                                        mDatabase = FirebaseDatabase.getInstance().getReference();
+                                        mDatabase.child("users").child(uid).addListenerForSingleValueEvent(
+                                                new ValueEventListener() {
+                                                    @Override
+                                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                                        // Get user value
+                                                        Object s = dataSnapshot.getValue();
+                                                        System.out.println(s.toString());
+
+                                                        // ...
+                                                    }
+
+                                                    @Override
+                                                    public void onCancelled(DatabaseError databaseError) {
+                                                        Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                                                    }
+                                                });
+                                        Intent intent = new Intent(getApplicationContext(), ChildView.class);
+                                        MainActivity.this.startActivity(intent);
+                                    } else {
+                                        System.out.println("Not logged in!");
+                                    }
+                                }
                             }
                         });
             }
         });
-
     }
-
 }
