003094a4cdc73506426e8a3edd1935936669713d
jsjqian <jack.qian97@gmail.com>
2016-07-07 11:24:46 -0400

    Referenced stuff in AddTask

--- app/src/main/java/com/example/jxw679/mogul/activities/AddTask.java
+++ app/src/main/java/com/example/jxw679/mogul/activities/AddTask.java
@@ -1,7 +1,10 @@
 package com.example.jxw679.mogul.activities;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.jxw679.mogul.R;
 
@@ -11,5 +14,13 @@ public class AddTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        EditText taskName = (EditText) findViewById(R.id.task_name_edit);
+        EditText description = (EditText) findViewById(R.id.description_edit);
+        EditText price = (EditText) findViewById(R.id.price_edit);
+        EditText deadline = (EditText) findViewById(R.id.deadline_edit);
+        ImageButton assign = (ImageButton) findViewById(R.id.add_child_to_task);
+        ImageButton backbutton = (ImageButton) findViewById(R.id.back_button);
+        ImageButton createTask = (ImageButton) findViewById(R.id.create_task_button);
     }
 }
