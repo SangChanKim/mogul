0359a37d1d9958486c23e2e25ff1fe04cf941024
jsjqian <jack.qian97@gmail.com>
2016-07-07 11:51:05 -0400

    Back buttons and swipe refresh available for everything that needs it

--- app/src/main/java/com/example/jxw679/mogul/activities/AddTask.java
+++ app/src/main/java/com/example/jxw679/mogul/activities/AddTask.java
@@ -1,8 +1,10 @@
 package com.example.jxw679.mogul.activities;
 
+import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -20,7 +22,14 @@ public class AddTask extends AppCompatActivity {
         EditText price = (EditText) findViewById(R.id.price_edit);
         EditText deadline = (EditText) findViewById(R.id.deadline_edit);
         ImageButton assign = (ImageButton) findViewById(R.id.add_child_to_task);
-        ImageButton backbutton = (ImageButton) findViewById(R.id.back_button);
+        ImageButton backButton = (ImageButton) findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ParentView.class);
+                startActivity(intent);
+            }
+        });
         ImageButton createTask = (ImageButton) findViewById(R.id.create_task_button);
     }
 }
--- app/src/main/java/com/example/jxw679/mogul/activities/ChildTask.java
+++ app/src/main/java/com/example/jxw679/mogul/activities/ChildTask.java
@@ -2,8 +2,11 @@ package com.example.jxw679.mogul.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.jxw679.mogul.R;
+
+import org.w3c.dom.Text;
 
 public class ChildTask extends AppCompatActivity {
 
@@ -11,5 +14,7 @@ public class ChildTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_task);
+
+        TextView description = (TextView) findViewById(R.id.description_text);
     }
 }
