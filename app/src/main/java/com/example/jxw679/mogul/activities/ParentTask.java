0357ff7247be5a47e4ab81b88eb8eebfc07862bb
prestonprice57 <preston.price@capitalone.com>
2016-07-07 09:36:51 -0400

    made some changes to parenttask

--- app/src/main/java/com/example/jxw679/mogul/activities/ParentTask.java
+++ app/src/main/java/com/example/jxw679/mogul/activities/ParentTask.java
@@ -2,13 +2,16 @@ package com.example.jxw679.mogul.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Spinner;
 
 import com.example.jxw679.mogul.R;
 
 public class ParentTask extends AppCompatActivity {
 
+    String[] statuses = {"Requested", "In Progress", "In Review", "Completed"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +20,16 @@ public class ParentTask extends AppCompatActivity {
         ImageButton back_button = (ImageButton) findViewById(R.id.back_button);
         TextView task_name = (TextView) findViewById(R.id.task_name_edit);
         TextView description_text = (TextView) findViewById(R.id.description_text);
+        Spinner status_spinner = (Spinner) findViewById(R.id.status_spinner);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, statuses);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        status_spinner.setAdapter(arrayAdapter);
+
+        for (String status : statuses) {
+
+        }
     }
 }
