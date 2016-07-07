9cfe81cc2af3052c951ca19710d7943ac63a0d3f
jsjqian <jack.qian97@gmail.com>
2016-07-06 21:07:59 -0400

    Parent View almost complete

--- app/src/main/java/com/example/jxw679/mogul/ParentView.java
+++ app/src/main/java/com/example/jxw679/mogul/ParentView.java
@@ -1,13 +1,81 @@
 package com.example.jxw679.mogul;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
 
 public class ParentView extends AppCompatActivity {
+
+    private ArrayList<String> data = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_view);
+        ListView lv = (ListView) findViewById(R.id.child_list);
+        generateListContent();
+        lv.setAdapter(new MyListAdapter(this, R.layout.child_list_item, data));
+    }
+
+    private void generateListContent() {
+
+
+    }
+
+    private class MyListAdapter extends ArrayAdapter {
+        private int layout;
+
+        private MyListAdapter(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            layout = resource;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder mainViewHolder = null;
+            if (convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.profile_pic = (ImageView) convertView.findViewById(R.id.profile_pic);
+                viewHolder.name = (TextView) convertView.findViewById(R.id.child_name);
+                viewHolder.balance = (TextView) convertView.findViewById(R.id.child_balance);
+                viewHolder.button = (ImageButton) convertView.findViewById(R.id.button);
+                viewHolder.button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //Send child ID or something
+                    }
+                });
+                convertView.setTag(viewHolder);
+            } else {
+                mainViewHolder = (ViewHolder) convertView.getTag();
+                mainViewHolder.name.setText((Integer) getItem(position));
+            }
+
+            return convertView;
+        }
+    }
+
+    public class ViewHolder {
+        ImageView profile_pic;
+        TextView name;
+        TextView balance;
+        ImageButton button;
     }
 }
